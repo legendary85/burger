5633f610892fb327f54289b298d44cc939da6512
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-09-17 17:06:50 -0500

    built shema and seed.sql with data

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burger
+  (burger_name, devoured)
+VALUES
+  ('Double Cheese Burger', true);
+
+INSERT INTO burger
+  (burger_name, devoured)
+VALUES
+  ('Tripple Cheese Burger', true);
+
+INSERT INTO burger
+  (burger_name, devoured)
+VALUES
+  ('Veggie Delight Cheese Burger', true);
