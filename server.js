0740f79b2e82686ac8ede0f73dc19c7fdb9603bb
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-09-18 15:41:28 -0500

    server set up on PORT 8080

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 //Dependencies
 const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
